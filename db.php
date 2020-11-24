cc78f08740cbd28f0aaa61fb52a698838208fdd9
shanuraj715 <shanuraj715@gmail.com>
2020-11-25 01:21:00 +0530

    First commit

--- db.php
+++ db.php
@@ -4,10 +4,6 @@ $db['host']         = 'localhost';
 $db['username']     = 'root';
 $db['password']     = 'vips';
 $db['db_name']      = 'new_djraj';
-// $db['host']         = '103.129.98.12';
-// $db['username']     = 'techfact_new_dj';
-// $db['password']     = 'shanuraj715';
-// $db['db_name']      = 'techfact_new_djraj';
 
 foreach($db as $key => $value){
     define(strtoupper($key), $value);
