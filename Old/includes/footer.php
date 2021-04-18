67e3ce2a418cf97cd4a52fb86f58347b7e93ce03
Shanu Raj <shanuraj715@gmail.com>
2021-04-18 19:21:47 +0530

    Delete Old directory

--- Old/includes/footer.php
+++
@@ -1,3 +0,0 @@
-<div class="footer">
-    <p class="footer_text">Copyright &copy; 2020 | <?php echo SITE_TITLE;?></p>
-</div>
--- Old/includes/header.php
+++
@@ -1,9 +0,0 @@
-<div class="header">
-    <!-- <p class="header_text"> -->
-    	<!-- <a class="header_link" href="<?php //echo SITE_URL; ?>"><?php //echo SITE_TITLE;?></a> -->
-    <!-- </p> -->
-    <a class="header_link" href="<?php echo SITE_URL; ?>">
-    	<img src="<?php echo SITE_URL . 'images/site_title_image.png';?>" class="header_site_title_img" alt="<?php echo SITE_TITLE . ' logo';?>">
-    </a>
-    <p class="header_desc">India's No. 1 Music Site</p>
-</div>
