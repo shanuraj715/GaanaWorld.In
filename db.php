7ec6e31b06d758cd1ad8462e79a0d665373ee47d
Shanu Raj <shanuraj715@gmail.com>
2022-03-05 22:29:13 +0530

    Added File Artwork

---
+++ db.php
@@ -0,0 +1,19 @@
+<?php
+
+$db['host']         = 'localhost';
+$db['username']     = 'root';
+$db['password']     = '';
+$db['db_name']      = 'gw';
+
+foreach($db as $key => $value){
+    define(strtoupper($key), $value);
+}
+
+$conn = mysqli_connect(HOST, USERNAME, PASSWORD, DB_NAME);
+
+
+if(!$conn){
+    die("Unable to connect to the database.");
+}
+
+?>
